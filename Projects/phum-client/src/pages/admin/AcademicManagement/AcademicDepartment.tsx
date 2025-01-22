345ff6886f9a47146485125d41292eef83057adb
Shipon Hossen Raju <msshipon234@gmail.com>
2025-01-22 12:43:38 +0600

    academic department complete

--- Projects/phum-client/src/pages/admin/AcademicManagement/AcademicDepartment.tsx
+++ Projects/phum-client/src/pages/admin/AcademicManagement/AcademicDepartment.tsx
@@ -1,3 +1,73 @@
+import { Table, TableColumnType, TableProps } from "antd";
+import { useState } from "react";
+import { useGetAcademicDepartmentsQuery } from "../../../redux/features/admin/academicManagement.api";
+import { TQueryParam } from "../../../types";
+
+export type TTableData = {
+  key: string;
+  name: string;
+  academicFaculty: string;
+};
+
 export default function AcademicDepartment() {
-  return <div>this is a AcademicDepartment</div>;
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+  const {
+    data: departmentData,
+    isLoading,
+    isFetching,
+  } = useGetAcademicDepartmentsQuery(params);
+
+  const tableData =
+    departmentData?.data?.map(({ _id, name, academicFaculty }) => ({
+      key: _id,
+      name: name,
+      academicFaculty: academicFaculty.name,
+    })) || [];
+
+  const columns: TableColumnType<TTableData>[] = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "name",
+      filters: [],
+    },
+    {
+      title: "Academic Faculty",
+      key: "academicFaculty",
+      dataIndex: "academicFaculty",
+      filters: [],
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams: TQueryParam[] = [];
+
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      filters.academicFaculty?.forEach((item) =>
+        queryParams.push({ name: "academicFaculty", value: item })
+      );
+
+      setParams(queryParams);
+    }
+  };
+
+  return (
+    <div>
+      {" "}
+      <Table
+        loading={isFetching || isLoading}
+        columns={columns}
+        dataSource={tableData}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
--- Projects/phum-client/src/pages/admin/AcademicManagement/CreateAcademicDepartment.tsx
+++ Projects/phum-client/src/pages/admin/AcademicManagement/CreateAcademicDepartment.tsx
@@ -1,14 +1,40 @@
 import { Button, Col, Row } from "antd";
 import PHFrom from "../../../components/form/PHFrom";
 import PHInput from "../../../components/form/PHInput";
-import { useAddAcademicDepartmentMutation } from "../../../redux/features/admin/academicManagement.api";
+import {
+  useAddAcademicDepartmentMutation,
+  useGetAcademicFacultiesQuery,
+} from "../../../redux/features/admin/academicManagement.api";
+import PHSelect from "../../../components/form/PHSelect";
+import { toast } from "sonner";
+
+// const departmentDummyData = {
+//   name: "Department of L1",
+//   academicFaculty: "65b00f3510b74fcbd7a25d86",
+// };
 
 export default function CreateAcademicDepartment() {
+  const {
+    data: facultyData,
+    isLoading,
+    isFetching,
+  } = useGetAcademicFacultiesQuery(undefined);
+
+  const facultyOptions =
+    facultyData?.data?.map(({ _id, name }) => ({ value: _id, label: name })) ||
+    [];
+
   const [addAcademicDepartment] = useAddAcademicDepartmentMutation(undefined);
-  const onSubmit = (data: any) => {
+
+  const onSubmit = async (data: any) => {
     console.log("submit data ", data);
 
-    addAcademicDepartment(data);
+    try {
+      await addAcademicDepartment(data).unwrap();
+      toast.success("Academic department created successfully");
+    } catch (error) {
+      toast.error("Failed to create academic department");
+    }
   };
 
   return (
@@ -17,6 +43,14 @@ export default function CreateAcademicDepartment() {
         <Row gutter={16}>
           <Col span={12}>
             <PHInput type="text" label="Department Name" name="name" />
+          </Col>
+          <Col span={12}>
+            <PHSelect
+              options={facultyOptions}
+              disabled={isLoading || isFetching}
+              label="Academic Faculty"
+              name="academicFaculty"
+            />
           </Col>
         </Row>
 
